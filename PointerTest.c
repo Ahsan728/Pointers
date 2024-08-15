9682fad5d0d28ead7734bb5c605803210f4543a9
Ahsan728 <ehsan.puc16@gmail.com>
2024-08-16 01:52:08 +0200

    Pointer Arithmatic

--- PointerTest.c
+++ PointerTest.c
@@ -5,11 +5,10 @@ int main ()
 {
 int a; int *p;
 a=10;
-p= &a; //&a address of a
-printf("Address of P is %d\n",p);
-printf("Value of P is %d\n",*p);
-int b=20;
-*p= b;//will the address in p change to point b
-printf("Address of P is %d\n",p);
-printf("Value of P is %d\n",*p);
+p= &a; 
+//Pointer Arithmatic
+printf("Address P is %d\n",p);
+printf("Size of integer is %d bytes\n",sizeof(int));
+printf("Address p+1 is %d\n",p+1);
+
 }

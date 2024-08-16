62a506eade1592a61c5936e554048ded5c4fe762
Ahsan728 <ehsan.puc16@gmail.com>
2024-08-16 02:25:06 +0200

    Pointer Arithmatic1

--- PointerTest.c
+++ PointerTest.c
@@ -8,7 +8,9 @@ a=10;
 p= &a; 
 //Pointer Arithmatic
 printf("Address P is %d\n",p);
+printf("Value at Address P is %d\n",*p);
 printf("Size of integer is %d bytes\n",sizeof(int));
 printf("Address p+1 is %d\n",p+1);
+printf("Value at Address P+1 is %d\n",*(p+1));
 
 }
